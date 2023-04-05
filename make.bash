9d7362daeaeeed613acea535b5b6c3420728e320
swarupsengupta2007 <swarupsengupta2007@gmail.com>
2023-04-05 18:28:17 +0530

    missing \

--- make.bash
+++ make.bash
@@ -5,8 +5,8 @@ BASE_VERION=3.16.5
 S6VERSION=3.1.4.2
 
 sudo docker buildx build \
-				--build-arg BASE_VERSION=${BASE_VERSION}
-				--build-arg S6VERSION=${S6VERSION}
+				--build-arg BASE_VERSION=${BASE_VERSION} \
+				--build-arg S6VERSION=${S6VERSION} \
 				-t swarupsengupta2007/alpine-s6:3.16.0 \
 				-t swarupsengupta2007/alpine-s6:latest \
 				--platform ${TARGETS} . $1
